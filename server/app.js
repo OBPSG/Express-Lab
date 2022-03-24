be71c7218b1f851734e7897d48f52f8e23b2f927
OBPSG <vbeasupergamer@gmail.com>
2022-03-24 14:19:15 -0500

    Got the form submission endpoint working (except the JSON isn't quite valid)

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
 // app.get("/", (req, res) => 
 // {
@@ -12,6 +14,23 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.post("/formsubmissions", (req, res) => {
+    // console.log(req.body.userName);
+    // console.log(req.body.postBody);
+    const Post = {
+        userName: req.body.userName,
+        body: req.body.postBody
+    };
+
+    let filePath = path.join (__dirname, "/posts.json");
+
+    fs.appendFileSync(filePath, JSON.stringify(Post));
+    
+    res.send("Your post has been received!")
+});
 
 app.listen(3000);
